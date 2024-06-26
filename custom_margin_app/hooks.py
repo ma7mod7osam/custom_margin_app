7ae5b3ee62b738dec8b5f4d381cb2e9e754b6358
ma7mod7osam <ma7mod7osam@gmail.com>
2024-06-26 23:45:20 +0300

    Initial commit for custom margin app

--- custom_margin_app/hooks.py
+++ custom_margin_app/hooks.py
@@ -7,6 +7,10 @@ app_description = "An app to apply custom margin rate on items"
 app_email = "ma7mod7osam@gmail.com"
 app_license = "MIT"
 
+# Skip asset build
+app_include_js = []
+app_include_css = []
+
 doc_events = {
     "Quotation": {
         "before_save": "custom_margin_app.custom_margin_app.doctype.custom_margin_app.custom_margin_app.apply_custom_margin"
